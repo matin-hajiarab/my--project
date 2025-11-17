38f63cc8626ee3fb4c3446a5de268aaac82d0f1b
matin-hajiarab <matin.hajiarab@gmail.com>
2025-11-17 20:56:11 +0330

    new update

--- index.php
+++ index.php
@@ -24,32 +24,35 @@
 <section class="products">
   <h3>محصولات پر فروش</h3>
   <div class="product-list">
-    <div class="product-card">
+    <a href="Products.php" class="product-card">
       <img src="https://petshoptehran.com/wp-content/uploads/2022/09/%D8%AA%D8%AE%D9%85-%D9%85%D8%B1%D8%BA%DB%8C-%D9%BE%D8%B1%D9%86%D8%AF%D9%87-%D8%B1%D8%A7%D9%81.jpg" alt="غذا پرنده" />
       <div class="product-info">
         <h4>راف – RAFF Fruttovo</h4>
         <p>غذای پرندگان زینتی با تخم مرغ و میوه</p>
         <span class="price">160,000 تومان</span>
       </div>
-    </div>
-    <div class="product-card">
+    </a>
+
+    <a href="Products.php" class="product-card">
       <img src="https://tehrantooti.com/wp-content/uploads/2025/07/Cage-gold-parrot-1.jpg" alt="قفس پرنده" />
       <div class="product-info">
         <h4>قفس پرندگان و طوطی مدل مسافرتی</h4>
         <p>قفس مناسب برای پرندگان زینتی عروس هلندی و کوتوله....</p>
         <span class="price">1.200,000 تومان</span>
       </div>
-    </div>
-    <div class="product-card">
+    </a>
+
+    <a href="Products.php" class="product-card">
       <img src="https://dkstatics-public.digikala.com/digikala-products/96066e5c4522498a5f6f707a888e3eca81e2d021_1736223358.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90" alt="اسباب بازی پرنده" />
       <div class="product-info">
         <h4>اسباب بازی پرندگان - زنجیر بازی</h4>
         <p>اسباب بازی عروس هلندی، کوتوله برزیلی، گرینچک، قناری، فنج، مرغ عشق و تمام پرندگان </p>
         <span class="price">70,000 تومان</span>
       </div>
-    </div>
+    </a>
   </div>
 </section>
+
 <?php
   include("footer.html")
   ?>
